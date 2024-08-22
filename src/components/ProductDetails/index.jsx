d340e0f903a289bda7b68eeaf022824c5d07118c
Mirraely <mirraelymoura@gmail.com>
2024-08-21 22:22:04 -0300

    inserts grid elements into the ProductDetails 's component

--- src/components/ProductDetails/index.jsx
+++ src/components/ProductDetails/index.jsx
@@ -3,10 +3,10 @@ import PropTypes from 'prop-types';
 import { useState } from 'react';
 import ProductsQuantity from '../ProductsQuantity';
 import {
-    Button, Form, FormGroup, Label, Input,
+    Container, Button, Form, FormGroup, Label, Input,
     Dropdown, DropdownToggle, DropdownMenu, DropdownItem,
     Card, CardBody, CardText,
-    Col
+    Row, Col
 } from 'reactstrap';
 
 const ProductDetails = ({ product }) => {
@@ -17,71 +17,84 @@ const ProductDetails = ({ product }) => {
 
     return (
         <ProductDetailsContainer>
-            <Form>
-                <h2>{product.name} Candleaf®</h2>
-                <figure className="d-flex justify-content-center image-box border-0 my-3 mx-1">
-                    <img src={product.image} alt="Image's product" className="candle-image"/>
-                </figure>
-                <div className="d-flex">
-                    <p className="products-price text-primary">{product.price}$</p>
-                    <ProductsQuantity />
-                </div>
-                <div>
-                    <FormGroup check>
-                        <Label check>
-                            <Input
-                                type="radio"
-                                name="purchaseOptions"
-                                checked={selectedOption === 'one-time'}
-                                onChange={() => setSelectedOption('one-time')}
-                            />
-                            One time purchase
-                        </Label>
-                    </FormGroup>
-                    <FormGroup check>
-                        <Label check>
-                            <Input
-                                type="radio"
-                                name="purchaseOptions"
-                                checked={selectedOption === 'subscribe'}
-                                onChange={() => setSelectedOption('subscribe')}
-                            />
-                            <div>Subscribe and delivery every</div>
-                            <Dropdown isOpen={dropdownOpen} toggle={toggleDropdown}>
-                                <DropdownToggle caret>
-                                    {deliveryInterval}
-                                </DropdownToggle>
-                                <DropdownMenu>
-                                    <DropdownItem onClick={() => setDeliveryInterval('2 weeks')}>2 weeks</DropdownItem>
-                                    <DropdownItem onClick={() => setDeliveryInterval('4 weeks')}>4 weeks</DropdownItem>
-                                    <DropdownItem onClick={() => setDeliveryInterval('6 weeks')}>6 weeks</DropdownItem>
-                                </DropdownMenu>
-                            </Dropdown>
-                            <small>
-                                Subscribe now and get the 10% discount on every recurring order. The discount will be applied at checkout.
-                                <a href="#details">See details</a>
-                            </small>
-                        </Label>
-                    </FormGroup>
-                </div>
-                <Button color="success" className="mt-3">
-                    <i className="bi bi-cart"></i> + Add to cart
-                </Button>
-            </Form>
-            <Card className="product-details-card p-1">
-                <CardBody>
-                    <CardText><strong>Wax:</strong></CardText>
-                    <CardText><strong>Fragrance:</strong> </CardText>
-                    <CardText><strong>Burning Time:</strong></CardText>
-                    <CardText><strong>Dimension:</strong></CardText>
-                    <CardText><strong>Weight:</strong> </CardText>
-                </CardBody>
-            </Card>
-            <Col>
-                <p>All hand-made with natural soy wax, Candleaf is made for your pleasure moments.</p>
-                <p>🚚 FREE SHIPPING</p>
-            </Col>
-
+            <Container className='mt-3 py-4'>
+                <Form>
+                    <Row className="justify-content-center">
+                        <Col xs="12" md="6" className="text-center order-md-1 order-2">
+                            <figure className="image-box border-0">
+                                <img src={product.image} alt="Image's product" className="candle-image img-fluid" />
+                            </figure>
+                        </Col>
+                        <Col xs="12" md="6" className="text-center mt-4 order-md-3 order-5">
+                            <p>
+                                All hand-made with natural soy wax, Candleaf is made for your pleasure moments.
+                            </p>
+                            <strong className="text-center text-primary">🚚 FREE SHIPPING</strong>
+                        </Col>
+                        <Col xs="12" md="6" className="order-md-2 order-1">
+                            <h2 className="product-title">{product.name} Candleaf®</h2>
+                        </Col>
+                        <Col xs="12" md="6" className="order-md-4 order-4">
+                            <p className="products-price text-primary h4">{product.price}$</p>
+                            <ProductsQuantity className="mb-3" />
+                            <FormGroup check className="mb-3">
+                                <Label check>
+                                    <Input
+                                        type="radio"
+                                        name="purchaseOptions"
+                                        checked={selectedOption === 'one-time'}
+                                        onChange={() => setSelectedOption('one-time')}
+                                    />
+                                    One time purchase
+                                </Label>
+                            </FormGroup>
+                            <FormGroup check className="mb-3">
+                                <Label check>
+                                    <Input
+                                        type="radio"
+                                        name="purchaseOptions"
+                                        checked={selectedOption === 'subscribe'}
+                                        onChange={() => setSelectedOption('subscribe')}
+                                    />
+                                    <div>Subscribe and delivery every</div>
+                                    <Dropdown isOpen={dropdownOpen} toggle={toggleDropdown}>
+                                        <DropdownToggle caret>
+                                            {deliveryInterval}
+                                        </DropdownToggle>
+                                        <DropdownMenu>
+                                            <DropdownItem onClick={() => setDeliveryInterval('2 weeks')}>
+                                                2 weeks
+                                            </DropdownItem>
+                                            <DropdownItem onClick={() => setDeliveryInterval('4 weeks')}>
+                                                4 weeks
+                                            </DropdownItem>
+                                            <DropdownItem onClick={() => setDeliveryInterval('6 weeks')}>
+                                                6 weeks
+                                            </DropdownItem>
+                                        </DropdownMenu>
+                                    </Dropdown>
+                                    <small>
+                                        Subscribe now and get the 10% discount on every recurring order.
+                                        The discount will be applied at checkout. <a href="#details">See details</a>
+                                    </small>
+                                </Label>
+                            </FormGroup>
+                            <Button color="success" className="w-100">
+                                <i className="bi bi-cart"></i> + Add to cart
+                            </Button>
+                            <Card className="product-details-card p-2 mt-4">
+                                <CardBody className="w-100">
+                                    <CardText><strong>Wax:</strong> {product.wax}</CardText>
+                                    <CardText><strong>Fragrance:</strong> {product.fragrance} </CardText>
+                                    <CardText><strong>Burning Time:</strong> {product.burningTime} </CardText>
+                                    <CardText><strong>Dimension:</strong> {product.dimension}</CardText>
+                                    <CardText><strong>Weight:</strong> {product.weight}</CardText>
+                                </CardBody>
+                            </Card>
+                        </Col>
+                    </Row>
+                </Form>
+            </Container>
         </ProductDetailsContainer>
     );
 };
