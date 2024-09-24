edc63fab7d36577f84f1d02355e9746f269f3154
Mirraely <mirraelymoura@gmail.com>
2024-09-24 01:08:53 -0300

    ajusta layout do componente products

--- src/components/Products/index.jsx
+++ src/components/Products/index.jsx
@@ -14,7 +14,7 @@ const Products = () => {
     const products = ProductsData;
 
     return (
-        <ProductsContainer className="mx-4 my-5 text-center">
+        <ProductsContainer className="mx-4 my-5 text-center py-1">
             <section className="mt-5">
                 <h1 className="mt-5 primary-2">Products</h1>
                 <p className="text-muted my-2 subtitle-products text-secondary">Order it for you or for your beloved ones</p>
