ce6133a4fa9efe7a804ecee0d53a638ea3f4d5db
Mirraely <mirraelymoura@gmail.com>
2024-09-25 20:56:04 -0300

    Adiciona icones ao formulário

--- src/components/PaymentForms/index.jsx
+++ src/components/PaymentForms/index.jsx
@@ -1,135 +1,179 @@
 import { Form, Link } from "react-router-dom";
-import { CardHeader, Col, FormGroup, Input, InputGroupText, Label, Row } from "reactstrap";
+import {
+  CardHeader,
+  Col,
+  FormGroup,
+  Input,
+  InputGroup,
+  InputGroupText,
+  Label,
+  Row,
+} from "reactstrap";
 import CreditCard from "../../assets/images/CreditCardFill.svg";
-import { Container, Card, CardBody } from 'reactstrap';
+import { Container, Card, CardBody } from "reactstrap";
 // import { faLock, faCreditCard } from '@fortawesome/free-solid-svg-icons';
+import { BsLockFill, BsInfoSquareFill } from "react-icons/bs";
 
 const Payment = () => {
+  return (
+    <Container className="mt-5 pb-5">
+      <Col md="6">
+        <Card className="mb-5 border-primary border-opacity-25">
+          <CardBody className="mx-2">
+            <Row>
+              <Col xs="2">
+                <h6 className="mb-3 text-secondary">Contact</h6>
+              </Col>
+              <Col xs="8">
+                <span>joe.spagnuolo@uxbly.com</span>
+              </Col>
+              <Col xs="2" className="text-end">
+                <Link to={"/order"} className="text-primary">
+                  Edit
+                </Link>
+              </Col>
 
-    return (
-        <Container className="mt-5 pb-5">
-            <Col md="6">
-                <Card className="mb-5 border-primary border-opacity-25">
-                    <CardBody className="mx-2">
-                        <Row>
-                            <Col xs="2">
-                                <h6 className="mb-3">Contact</h6>
-                            </Col>
-                            <Col xs="8">
-                                <span>joe.spagnuolo@uxbly.com</span>
-                            </Col>
-                            <Col xs="2" className="text-end">
-                                <Link to={'/order'} className="text-primary">
-                                    Edit
-                                </Link>
-                            </Col>
+              <hr className="border-primary border-opacity-50" />
 
-                            <hr className="border-primary border-opacity-50" />
+              <Col xs="2">
+                <h6 className="mb-3 text-secondary">Ship to</h6>
+              </Col>
+              <Col xs="8">
+                <span>
+                  Via Firenze 23, 92023, Campobello di Licata AG, Itália
+                </span>
+              </Col>
+              <Col xs="2" className="text-end">
+                <Link to={"/order"} className="text-primary">
+                  Edit
+                </Link>
+              </Col>
 
-                            <Col xs="2">
-                                <h6 className="mb-3">Ship to</h6>
-                            </Col>
-                            <Col xs="8">
-                                <span>Via Firenze 23, 92023, Campobello
-                                    di Licata AG, Itália
-                                </span></Col>
-                            <Col xs="2" className="text-end">
-                                <Link to={'/order'} className="text-primary">
-                                    Edit
-                                </Link>
-                            </Col>
+              <hr className="border-primary border-opacity-50" />
 
-                            <hr className="border-primary border-opacity-50" />
+              <Col xs="2">
+                <h6 className="text-secondary">Method</h6>
+              </Col>
+              <Col xs="8">
+                <span>
+                  Standard Shipping
+                  <span className="fw-light"> - FREE</span>
+                </span>
+              </Col>
+              <Col xs="2" className="text-end">
+                <Link to={"/order"} className="text-primary">
+                  Edit
+                </Link>
+              </Col>
+            </Row>
+          </CardBody>
+        </Card>
 
-                            <Col xs="2">
-                                <h6 className="">Method</h6>
-                            </Col>
-                            <Col xs="8">
-                                <span>Standard Shipping -
-                                    <span>FREE</span>
-                                </span></Col>
-                            <Col xs="2" className="text-end">
-                                <Link to={'/order'} className="text-primary">
-                                    Edit
-                                </Link>
-                            </Col>
-                        </Row>
-                    </CardBody>
-                </Card>
+        <h5 className="mb-4">Payment method</h5>
+        <Card className="mb-2 border-primary border-opacity-25">
+          <CardHeader className="d-flex gap-4">
+            <img src={CreditCard} alt="CreditCard" />
+            <h5 className="text-primary">Credit Card</h5>
+          </CardHeader>
+          <CardBody>
+            <Form>
+              <FormGroup>
+                <InputGroup>
+                  <Input
+                    type="text"
+                    name="cardNumber"
+                    id="cardNumber"
+                    placeholder="Card Number"
+                    className="border-end-0"
+                  />
+                  <InputGroupText className="bg-white">
+                    <BsLockFill className="text-secondary" /> {/* Icon*/}
+                  </InputGroupText>
+                </InputGroup>
+              </FormGroup>
 
-                <h5 className="mb-4">Payment method</h5>
-                <Card className="mb-2 border-primary border-opacity-25">
-                    <CardHeader className="d-flex gap-4">
-                        <img src={CreditCard} alt="CreditCard" />
-                        <h5 className="text-primary">Credit Card</h5>
-                    </CardHeader>
-                    <CardBody>
-                        <Form>
-                            <FormGroup>
-                                <Input id="cardNumber" name="cardNumber"
-                                    placeholder="Card Number" type="nnumber" />
-                                <InputGroupText>
-                                    <faLock/>
-                                </InputGroupText>
-                            </FormGroup>
-                            <FormGroup>
-                                <Input id="holderNumber" name="name"
-                                    placeholder="Holder Number" type="text"
-                                    className="" />
-                            </FormGroup>
-                            <div className="d-flex gap-3">
-                                <FormGroup className="w-50">
-                                    <Input id="expiration" name="expiration"
-                                        placeholder="Expiration (MM/YY)" type="date" />
-                                </FormGroup>
-                                <FormGroup className="w-50">
-                                    <Input id="cvv" name="cvv"
-                                        placeholder="CVV" type="password"
-                                        className="" />
-                                </FormGroup>
-                            </div>
-                        </Form>
-                    </CardBody>
-                </Card>
+              <FormGroup>
+                <Input
+                  id="holderName"
+                  name="name"
+                  placeholder="Holder Name"
+                  type="text"
+                  className=""
+                />
+              </FormGroup>
 
-                <h5 className="mb-4">Billing address</h5>
-                <Card className="mb-2 border-primary border-opacity-25">
-                    <CardBody className="mx-2">
-                        <Row>
-                            <Col xs="8">
-                                <FormGroup check>
-                                    <Label check>
-                                        <Input type="radio" name="shippingMethod" defaultChecked />{' '}
-                                        <span className="ms-3">Same as the shipping address</span>
-                                    </Label>
-                                </FormGroup>
-                            </Col>
+              <div className="d-flex gap-3">
+                <FormGroup className="w-50">
+                    <Input
+                      id="expiration"
+                      name="expiration"
+                      placeholder="Expiration (MM/YY)"
+                      type="text"
+                    />
+                  
+                </FormGroup>
+                <FormGroup className="w-50">
+                <InputGroup>
+                  <Input
+                    id="cvv"
+                    name="cvv"
+                    placeholder="CVV"
+                    type="password"
+                    className="border-end-0"
+                  />
+                    <InputGroupText className="bg-white">
+                      <BsInfoSquareFill className="text-secondary"/> {/* Ícon */}
+                    </InputGroupText>
+                  </InputGroup>
+                </FormGroup>
+              </div>
+            </Form>
+          </CardBody>
+        </Card>
 
-                            <hr className="border-primary border-opacity-50" />
+        <h5 className="mb-4">Billing address</h5>
+        <Card className="mb-2 border-primary border-opacity-25">
+          <CardBody className="mx-2">
+            <Row>
+              <Col xs="8">
+                <FormGroup check>
+                  <Label check>
+                    <Input type="radio" name="shippingMethod" defaultChecked />{" "}
+                    <span className="ms-3">Same as the shipping address</span>
+                  </Label>
+                </FormGroup>
+              </Col>
 
-                            <Col xs="8">
-                                <FormGroup check>
-                                    <Label check>
-                                        <Input type="radio" name="shippingMethod" />{' '}
-                                        <span className="ms-3">Use a different address for billing</span>
-                                    </Label>
-                                </FormGroup>
-                            </Col>
-                        </Row>
-                    </CardBody>
-                </Card>
+              <hr className="border-primary border-opacity-50" />
 
-                <div className="d-flex flex-column align-items-center gap-4 pt-5">
-                    <Link to={'/order'} className="w-100 btn btn-primary">
-                        Go to payment
-                    </Link>
-                    <Link to={'/order'} className="w-100 text-primary text-decoration-underline d-flex justify-content-center">
-                        Back to details
-                    </Link>
-                </div>
-            </Col>
-        </Container >
-    );
+              <Col xs="8">
+                <FormGroup check>
+                  <Label check>
+                    <Input type="radio" name="shippingMethod" />{" "}
+                    <span className="ms-3">
+                      Use a different address for billing
+                    </span>
+                  </Label>
+                </FormGroup>
+              </Col>
+            </Row>
+          </CardBody>
+        </Card>
+
+        <div className="d-flex flex-column align-items-center gap-4 pt-5">
+          <Link to={"/order"} className="w-100 btn btn-primary">
+            Go to payment
+          </Link>
+          <Link
+            to={"/order"}
+            className="w-100 text-primary text-decoration-underline d-flex justify-content-center"
+          >
+            Back to details
+          </Link>
+        </div>
+      </Col>
+    </Container>
+  );
 };
 
 export default Payment;
