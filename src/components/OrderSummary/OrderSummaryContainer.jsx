70b0212ad442740dd025cc3c1638f389d09dfc83
Mirraely <mirraelymoura@gmail.com>
2024-09-30 22:40:15 -0300

    Ajusta Layout do OrderSummary

--- src/components/OrderSummary/OrderSummaryContainer.jsx
+++ src/components/OrderSummary/OrderSummaryContainer.jsx
@@ -1,65 +1,105 @@
 import styled from "styled-components";
 
 const OrderSummaryContainer = styled.section`
-/* max-height: 51.25em; */
+  /* max-height: 51.25em; */
 
-.rigth-order-summary-section,
-.accordion-item, 
-.table>:not(caption)>*>* {
-    background-color:  #F2F2F2;
-}
+  .rigth-order-summary-section,
+  .accordion-item,
+  .table > :not(caption) > * > * {
+    background-color: #f2f2f2;
+  }
 
-.accordion-item:first-of-type {
+  .accordion-item:first-of-type {
     border-radius: initial;
-}
+  }
 
-.accordion-header {
+  .accordion-header {
     padding: 0.4em 0;
-}
+  }
 
-.accordion-button:focus {
+  .accordion-button:focus {
     box-shadow: none;
-}
+  }
 
-table tr {
+  .text-16 {
+    font-size: 1em;
+    font-weight: 500;
+    letter-spacing: -0.9px;
+  }
+
+  .price {
+    font-size: 1.3125em;
+    font-weight: 600;
+    letter-spacing: -0.9px;
+    line-height: 2.5em;
+  }
+
+  .lg-price {
+    font-family: 'Roboto', sans-serif;
+    font-style: normal;
+    font-weight: 500; 
+    font-size: 1.5em;
+    letter-spacing: -0.9px;
+    color: black;
+  }
+
+  table tr {
     border-color: rgba(86, 178, 128, 0.2);
-}
+  }
 
-.accordion-button {
-    background-color: #F2F2F2;
+  .accordion-button {
+    background-color: #f2f2f2;
     display: flex;
     justify-content: space-between;
-}
+  }
 
-.accordion-button::after {
+  .accordion-button::after {
     display: none;
-}
+  }
 
-.accordion-expander {
+  .accordion-expander {
     transition: var(--bs-accordion-btn-icon-transition);
-}
+  }
 
-.accordion-expander:not(.collapsed) {
+  .accordion-expander:not(.collapsed) {
     transform: var(--bs-accordion-btn-icon-transform);
-}
+  }
 
-.form-control {
+  .form-control {
     border-radius: initial;
-}
+  }
 
-.form-control,
-.btn {
+  .form-control,
+  .btn {
     border-color: #898989;
+  }
+
+  .btn-secondary {
+    background-color: #a8a8a8e1;
+  }
+
+  .image-box {
+    background-color: #f7f8fa;
+  }
+
+.add-button {
+    font-size: 1em;
+    font-family: 'Roboto', sans-serif;
+    font-weight: 300;
+    letter-spacing: 0px;
+    padding: 0.47em 1.3em;
 }
 
-.btn-secondary {
-    background-color: #A8A8A8;
+.third-block {
+    font-family: 'Roboto', sans-serif;
+    font-style: normal;
+    font-weight: 500; 
+    font-size: 0.875em;
+    letter-spacing: -0.9px;
+    color: #616161;
 }
 
-.image-box {
-    background-color: #F7F8FA;
-}
 
- `;
+`;
 
 export default OrderSummaryContainer;
--- src/components/OrderSummary/index.jsx
+++ src/components/OrderSummary/index.jsx
@@ -1,5 +1,5 @@
 import OrderSummaryContainer from "./OrderSummaryContainer";
-import { Button, Col, Form, FormGroup, Input, Table } from "reactstrap";
+import { Button, Col, Form, FormGroup, Input, Row, Table } from "reactstrap";
 import PropTypes from "prop-types";
 import ProductsData from "../../db/ProductData";
 
@@ -7,68 +7,76 @@ const OrderSummary = ({ className }) => {
   const product = ProductsData.find((p) => p.id == 1);
   return (
     <OrderSummaryContainer className={`${className}`}>
-      <Col
-        md="12"
-        className="rigth-order-summary-section py-5 pe-5"
-      >
-        <section className="pe-5 me-5">
-          <Table className="mx-5">
+      <Col md="12" className="rigth-order-summary-section py-5">
+        <section className="px-5">
+          <Table className="ms-2">
             <thead>
               <tr className="mb-5">
-                <th scope="col-2">
-                  <figure className="image-box w-75 border-0 d-flex flex-column-reverse position-relative">
-                    <img
-                      src={product.image}
-                      alt="Image's product"
-                      className="candle-image img-fluid"
-                    />
-                    <span className="position-absolute top-0 start-100 translate-middle px-2 bg-primary border border-light rounded-circle text-white">
-                      1
-                    </span>
-                  </figure>
-                </th>
-                <th
-                  scope="col-10"
-                  className="d-flex flex-column border-0 gap-3 px-0 mx-0 text-start"
-                >
-                  <h5 className="product-title">{product.name} Candleaf®</h5>
-                  <p className="  text-primary">${product.price}</p>
+                <th colSpan={2} className="pb-4">
+                  <Row>
+                    <Col md="5">
+                      <figure className="image-box w-100 border-0 d-flex flex-column-reverse position-relative">
+                        <img
+                          src={product.image}
+                          alt="Image's product"
+                          className="candle-image img-fluid"
+                        />
+                        <span className="position-absolute top-0 start-100 translate-middle px-2 bg-primary border border-light rounded-circle text-white">
+                          1
+                        </span>
+                      </figure>
+                    </Col>
+                    <Col md="7" className="ps-5">
+                      <h5 className="product-title text-16">
+                        {product.name} Candleaf®
+                      </h5>
+                      <p className="text-primary price">${product.price}</p>
+                    </Col>
+                  </Row>
                 </th>
               </tr>
             </thead>
             <tbody>
-              <tr scope="row">
-                <td colSpan="2">
+              <tr scope="row" className="gx-0">
+                <td colSpan="2" className="pt-4 pb-2 px-0">
                   <Form>
                     <FormGroup className="d-flex w-100 gap-2">
-                      <Input
-                        type="text"
-                        name="coupon"
-                        id="couponCode"
-                        placeholder="Enter coupon code"
-                        className="w-75"
-                      />
-                      <Button color="secondary" className="border-0">
-                        Add code
-                      </Button>
+                      <Col md="9">
+                        <Input
+                          type="text"
+                          name="coupon"
+                          id="couponCode"
+                          placeholder="Coupon code"
+                        />
+                      </Col>
+                      <Col md="3" className="gx-0">
+                        <Button
+                          color="secondary"
+                          className="border-0 add-button"
+                        >
+                          Add code
+                        </Button>
+                      </Col>
                     </FormGroup>
                   </Form>
                 </td>
               </tr>
-              <tr scope="row">
-                <td scope="row" className="">
+              <tr scope="row" className="third-block">
+                <td scope="row" className="px-0 pt-4 pb-3">
                   <p>Subtotal</p>
                   <p>Shipping</p>
                 </td>
-                <td scope="col" className="text-end">
-                  <p>$9.99</p>
+                <td scope="col" className="text-end px-0 pt-4 pb-3">
+                  <p>$ 9.99</p>
                   <p>Calculated at the next step</p>
                 </td>
               </tr>
               <tr scope="row" className="">
-                <td scope="col">Total</td>
-                <td scope="col" className="text-end">
-                  <strong>$9.99</strong>
+                <td scope="col" className="px-0 third-block pt-3 border-bottom-0">
+                  Total
+                </td>
+                <td scope="col" className="text-end px-0 pt-3 border-bottom-0 ">
+                  <strong className="lg-price">$ 9.99</strong>
                 </td>
               </tr>
             </tbody>
