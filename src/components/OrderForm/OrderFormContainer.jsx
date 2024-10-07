6ec2f1b26fc2a5f0ca935211791fa3d9cacab5e4
Mirraely <mirraelymoura@gmail.com>
2024-10-07 20:39:24 -0300

    Ajusta altura do select do formulário de pedido.

--- src/components/OrderForm/OrderFormContainer.jsx
+++ src/components/OrderForm/OrderFormContainer.jsx
@@ -17,18 +17,6 @@ const OrderFormContainer = styled.section`
     border: 0.25px solid var(--bd-border-form-grey);
   }
 
-  /* .custom-select {
-  appearance: none; Remove o estilo padrão do select
-  -webkit-appearance: none; Para navegadores baseados em WebKit
-  background-image: url('data:image/svg+xml;charset=utf8,%3Csvg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16"%3E%3Cpath fill="currentColor"d="M4 5h8a1 1 0 0 1 1 1v9a1 1 0 0 1-1 1H4a1 1 0 0 1-1-1V6a1 1 0 0 1 1-1z"/%3E%3C/svg%3E');
-  background-repeat: no-repeat;
-  background-position-x: calc(100% - 0.75rem);
-  background-position-y: center;
-  background-size: 16px 12px;
-  padding-right: 2rem; Espaço para a seta
-  border-bottom: 1px solid #ccc; Linha inferior
-} */
-
   .form-title {
     font-family: "Roboto", sans-serif;
     letter-spacing: -0.9px;
@@ -42,19 +30,31 @@ const OrderFormContainer = styled.section`
   }
 
   .form-floating > label {
-    font-size: 0.8em;
-    padding: 0.5rem 0.5rem;
+    font-size: 0.625rem;
+    padding: 0.9rem 0.5rem;
   }
 
-  /* .form-floating>.form-select {
-    min-height: calc(2.1rem + calc(var(--bs-border-width) * 2));
-    height: 0;
-  } */
+  .form-floating>.form-select~label::after {
+    background-color: transparent;
+  }
+
+  .form-floating>.form-select {
+    min-height: 2.5rem;
+    height: 2.5rem;
+    padding-top: 0.375rem;
+    padding-bottom: 0;
+  }
 
   input,
   select {
     font-size: 0.875em;
   }
+
+  input[type=text] {
+    min-height: 2.5rem;
+    height: 2.5rem;
+  }
+
 
   .span-text-input {
     vertical-align: text-top;
